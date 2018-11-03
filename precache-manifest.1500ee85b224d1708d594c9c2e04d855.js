450d22c45d573a7be504da05046bc16a945e9739
Raul Duran <rauladuran@hotmail.com>
2018-11-02 23:21:02 -0700

    Updates

---
+++ precache-manifest.1500ee85b224d1708d594c9c2e04d855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "7e786c860f5ad319c343",
+    "url": "/drum-machine/static/js/main.7e786c86.chunk.js"
+  },
+  {
+    "revision": "a93cec8a557ee382682a",
+    "url": "/drum-machine/static/js/1.a93cec8a.chunk.js"
+  },
+  {
+    "revision": "7e786c860f5ad319c343",
+    "url": "/drum-machine/static/css/main.8e197bee.chunk.css"
+  },
+  {
+    "revision": "4d3ea7a5195a3946bd2f052e32db6bbf",
+    "url": "/drum-machine/index.html"
+  }
+];
